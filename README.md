8d7930af820966fcff61441e7b86938505b69a57
Air-borne <sakyibernard77@gmail.com>
2022-11-22 17:37:05 +0000

    Add hash to printf line in readme

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# printf
+## printf
 <h1>ALX group of two project on the printf function in C;
---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef "MAIN_H"
+#define "MAIN_H"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* printf projects prototypes
+* Authors ; Bernard B.Sakyi and Hope Davids
+*/
+
+int _printf(const char *format, ...);
+
+#endif 
