265e3673b780f7782108c1787bf2386bae28bc47
Air-borne <sakyibernard77@gmail.com>
2022-11-23 05:55:29 +0000

    First push project

--- main.h
+++ main.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 
 /**
 * printf projects prototypes
@@ -10,5 +11,16 @@
 */
 
 int _printf(const char *format, ...);
+int _strlen(const char *str);
+int print_char(va_list list);
+int handler(const char *str, va_list);
+int percent_handler(const char *str, va_list list, int *i);
+int _putchar(char c)
+
+typedef struct _format
+{
+	char type;
+	int (*f)(va_list);
+} format;
 
 #endif 
